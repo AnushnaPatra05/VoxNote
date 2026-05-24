7a2306dac7d6c86b18c7c3a215a3700290c4adb9
AnushnaPatra05 <anushnaig@gmail.com>
2026-05-24 23:37:35 +0530

    auth middleware

---
+++ utils/middleware/authMiddleware.js
@@ -0,0 +1,33 @@
+const { verifyToken } = require('../utils/jwt');
+const User = require('../models/user');
+
+const protect = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return res.status(401).json({ message: 'No token provided. Access denied.' });
+    }
+
+    const token = authHeader.split(' ')[1];
+    const decoded = verifyToken(token);
+
+    const user = await User.findById(decoded.id).select('-password');
+    if (!user) {
+      return res.status(401).json({ message: 'User no longer exists.' });
+    }
+
+    req.user = user;
+    next();
+  } catch (error) {
+    if (error.name === 'TokenExpiredError') {
+      return res.status(401).json({ message: 'Token expired. Please log in again.' });
+    }
+    if (error.name === 'JsonWebTokenError') {
+      return res.status(401).json({ message: 'Invalid token.' });
+    }
+    next(error);
+  }
+};
+
+module.exports = { protect };
